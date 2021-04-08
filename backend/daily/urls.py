67c3be80d6a0c25070739eeba3c761ff9fb1ee84
ryonasso <ryounaoyuki@gmail.com>
2021-04-08 20:40:08 +0900

    nc

--- backend/daily/urls.py
+++ backend/daily/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListDaily.as_view()),
-    path('create/', views.CreatelDaily.as_view()),  # 日報一覧
+    path('', views.ListDaily.as_view()),  # 日報一覧
+    path('create/', views.CreatelDaily.as_view()),
     path('<int:pk>/', views.DetailDaily.as_view()),  # 1日の詳細
     path('<str:cat>/', views.CategoryDairy.as_view()),  # カテゴリ別一覧
 ]
