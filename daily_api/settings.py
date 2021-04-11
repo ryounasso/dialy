d174b272f2310d901b5127e9bec3bda8a0c2bfb7
ryonasso <ryounaoyuki@gmail.com>
2021-04-11 21:25:27 +0900

    nc

--- daily_api/settings.py
+++ daily_api/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.django.GzipManifestStaticFilesStorage',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
