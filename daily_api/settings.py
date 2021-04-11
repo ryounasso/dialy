e71a5c145286591589986883c0263583334ff33b
ryonasso <ryounaoyuki@gmail.com>
2021-04-11 17:07:02 +0900

    fix : fix error

--- daily_api/settings.py
+++ daily_api/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.django.GzipManifestStaticFilesStorage',
 ]
 
 # CORS_ORIGIN_WHITELIST = (
