be8f3e2db1950f9bee9f048a4f493755085e84f0
ryonasso <ryounaoyuki@gmail.com>
2021-04-12 13:52:16 +0900

    fix

--- daily_api/settings.py
+++ daily_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from socket import gethostname
-from os import environ
+import environ
 
 env = environ.Env()
 
