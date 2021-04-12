a680c373f400095746b35245ae32557845425072
ryonasso <ryounaoyuki@gmail.com>
2021-04-12 15:49:59 +0900

    fix : fix datebase

--- daily_api/settings.py
+++ daily_api/settings.py
@@ -119,12 +119,8 @@ WSGI_APPLICATION = 'daily_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'daily',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST':'',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
